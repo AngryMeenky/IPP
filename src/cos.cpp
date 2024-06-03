0cd77b55cd1ba810f894ab2c540de622e8b4b24d
John Powell <john_powell@partech.com>
2024-06-03 14:26:40 -0400

    Uses more appropriately named macros

--- src/cos.cpp
+++ src/cos.cpp
@@ -7,17 +7,17 @@ using namespace ipp;
 using namespace godot;
 
 
-IPP_OP_I(cos_32f_A11, ippsCos_32f_A11, 32F, 32f, 32F, 32f)
-IPP_OP_I(cos_32f_A21, ippsCos_32f_A21, 32F, 32f, 32F, 32f)
-IPP_OP_I(cos_32f_A24, ippsCos_32f_A24, 32F, 32f, 32F, 32f)
-IPP_OP_I(cos_64f_A26, ippsCos_64f_A26, 64F, 64f, 64F, 64f)
-IPP_OP_I(cos_64f_A50, ippsCos_64f_A50, 64F, 64f, 64F, 64f)
-IPP_OP_I(cos_64f_A53, ippsCos_64f_A53, 64F, 64f, 64F, 64f)
+IPP_UOP(cos_32f_A11, ippsCos_32f_A11, 32F, 32f, 32F, 32f)
+IPP_UOP(cos_32f_A21, ippsCos_32f_A21, 32F, 32f, 32F, 32f)
+IPP_UOP(cos_32f_A24, ippsCos_32f_A24, 32F, 32f, 32F, 32f)
+IPP_UOP(cos_64f_A26, ippsCos_64f_A26, 64F, 64f, 64F, 64f)
+IPP_UOP(cos_64f_A50, ippsCos_64f_A50, 64F, 64f, 64F, 64f)
+IPP_UOP(cos_64f_A53, ippsCos_64f_A53, 64F, 64f, 64F, 64f)
 
-IPP_OP_I(cos_32fc_A11, ippsCos_32fc_A11, 32FC, 32fc, 32FC, 32fc)
-IPP_OP_I(cos_32fc_A21, ippsCos_32fc_A21, 32FC, 32fc, 32FC, 32fc)
-IPP_OP_I(cos_32fc_A24, ippsCos_32fc_A24, 32FC, 32fc, 32FC, 32fc)
-IPP_OP_I(cos_64fc_A26, ippsCos_64fc_A26, 64FC, 64fc, 64FC, 64fc)
-IPP_OP_I(cos_64fc_A50, ippsCos_64fc_A50, 64FC, 64fc, 64FC, 64fc)
-IPP_OP_I(cos_64fc_A53, ippsCos_64fc_A53, 64FC, 64fc, 64FC, 64fc)
+IPP_UOP(cos_32fc_A11, ippsCos_32fc_A11, 32FC, 32fc, 32FC, 32fc)
+IPP_UOP(cos_32fc_A21, ippsCos_32fc_A21, 32FC, 32fc, 32FC, 32fc)
+IPP_UOP(cos_32fc_A24, ippsCos_32fc_A24, 32FC, 32fc, 32FC, 32fc)
+IPP_UOP(cos_64fc_A26, ippsCos_64fc_A26, 64FC, 64fc, 64FC, 64fc)
+IPP_UOP(cos_64fc_A50, ippsCos_64fc_A50, 64FC, 64fc, 64FC, 64fc)
+IPP_UOP(cos_64fc_A53, ippsCos_64fc_A53, 64FC, 64fc, 64FC, 64fc)
 
--- src/sin.cpp
+++ src/sin.cpp
@@ -7,17 +7,17 @@ using namespace ipp;
 using namespace godot;
 
 
-IPP_OP_I(sin_32f_A11, ippsSin_32f_A11, 32F, 32f, 32F, 32f)
-IPP_OP_I(sin_32f_A21, ippsSin_32f_A21, 32F, 32f, 32F, 32f)
-IPP_OP_I(sin_32f_A24, ippsSin_32f_A24, 32F, 32f, 32F, 32f)
-IPP_OP_I(sin_64f_A26, ippsSin_64f_A26, 64F, 64f, 64F, 64f)
-IPP_OP_I(sin_64f_A50, ippsSin_64f_A50, 64F, 64f, 64F, 64f)
-IPP_OP_I(sin_64f_A53, ippsSin_64f_A53, 64F, 64f, 64F, 64f)
+IPP_UOP(sin_32f_A11, ippsSin_32f_A11, 32F, 32f, 32F, 32f)
+IPP_UOP(sin_32f_A21, ippsSin_32f_A21, 32F, 32f, 32F, 32f)
+IPP_UOP(sin_32f_A24, ippsSin_32f_A24, 32F, 32f, 32F, 32f)
+IPP_UOP(sin_64f_A26, ippsSin_64f_A26, 64F, 64f, 64F, 64f)
+IPP_UOP(sin_64f_A50, ippsSin_64f_A50, 64F, 64f, 64F, 64f)
+IPP_UOP(sin_64f_A53, ippsSin_64f_A53, 64F, 64f, 64F, 64f)
 
-IPP_OP_I(sin_32fc_A11, ippsSin_32fc_A11, 32FC, 32fc, 32FC, 32fc)
-IPP_OP_I(sin_32fc_A21, ippsSin_32fc_A21, 32FC, 32fc, 32FC, 32fc)
-IPP_OP_I(sin_32fc_A24, ippsSin_32fc_A24, 32FC, 32fc, 32FC, 32fc)
-IPP_OP_I(sin_64fc_A26, ippsSin_64fc_A26, 64FC, 64fc, 64FC, 64fc)
-IPP_OP_I(sin_64fc_A50, ippsSin_64fc_A50, 64FC, 64fc, 64FC, 64fc)
-IPP_OP_I(sin_64fc_A53, ippsSin_64fc_A53, 64FC, 64fc, 64FC, 64fc)
+IPP_UOP(sin_32fc_A11, ippsSin_32fc_A11, 32FC, 32fc, 32FC, 32fc)
+IPP_UOP(sin_32fc_A21, ippsSin_32fc_A21, 32FC, 32fc, 32FC, 32fc)
+IPP_UOP(sin_32fc_A24, ippsSin_32fc_A24, 32FC, 32fc, 32FC, 32fc)
+IPP_UOP(sin_64fc_A26, ippsSin_64fc_A26, 64FC, 64fc, 64FC, 64fc)
+IPP_UOP(sin_64fc_A50, ippsSin_64fc_A50, 64FC, 64fc, 64FC, 64fc)
+IPP_UOP(sin_64fc_A53, ippsSin_64fc_A53, 64FC, 64fc, 64FC, 64fc)
 
